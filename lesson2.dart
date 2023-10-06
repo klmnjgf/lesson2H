00102b0d67f58d4f4fd8d2540c87f4a7a1b86631
klmnjgf <abek07898@gmail.com>
2023-10-06 20:40:30 +0600

    first commit

---
+++ lesson2.dart
@@ -0,0 +1,87 @@
+import 'dart:ffi';
+
+void main(List<String> args) {
+  int a = 20;
+  int b = 20;
+  print(a + b);
+  int c = a + b;
+  print(c);
+
+  int e = a * b;
+  print(e);
+
+  int f = a ~/ b;
+  print(f);
+
+  double a1 = 5.5;
+  double b1 = 2.2;
+  double c1 = a1 / b1;
+  print(c1);
+
+  int a2 = 20;
+  int b2 = 20;
+  print(a2 > b2);
+  print(a2 < b2);
+  print(a2 >= b2);
+  print(a2 <= b2);
+  print(a2 == b2);
+  print(a2 != b2);
+
+  num a1 = 15.5;
+  num a2 = 25;
+  num a3 = 30.7;
+  num a4 = 40;
+  num a5 = 12.2;
+  print(a1);
+  print(a2);
+  print(a3);
+  print(a4);
+  print(a5);
+
+  int b1 = 23;
+  int b2 = 44;
+  int b3 = 79;
+  int b4 = 32;
+  int b5 = 98;
+  print(b1);
+  print(b2);
+  print(b3);
+  print(b4);
+  print(b5);
+
+  double c1 = 13.3;
+  double c2 = 37076674;
+  double c3 = 67;
+  double c4 = 456;
+  double c5 = 8989;
+  print(c1);
+  print(c2);
+  print(c3);
+  print(c4);
+  print(c5);
+
+  // print(a2 + b4 + c1);
+
+  String name1 = "ais";
+  String name2 = "Bayana";
+  String name3 = "Aizirek";
+  String name4 = "Gulya";
+  String name5 = "Akber-Alkak";
+  print(name1);
+  print(name2);
+  print(name3);
+  print(name4);
+  print(name5);
+
+  List array1 = [23, 03, 200.3, 34.5, 56875.3, "bekaisuluu"];
+  List array2 = [65785, 6766, 86876, 7, 'ultim'];
+  List array3 = [8787, 787, 456];
+  List array4 = [12, 4556, 56789, 678];
+  List array5 = [23.5, 877.5, 'bbay'];
+  print(array1);
+  print(array2);
+  print(array3);
+  print(array4);
+  print(array5);
+  */
+}
